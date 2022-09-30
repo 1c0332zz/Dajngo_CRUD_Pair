0f5fc74c51ce80def513a34474db20ca54abffbd
kleenex1 <curian48@naver.com>
2022-09-30 14:42:24 +0900

    완성

--- reviews/urls.py
+++ reviews/urls.py
@@ -7,7 +7,11 @@ urlpatterns = [
     # - 리뷰 내용 보기
     path("reviews/<int:review_id>", views.detail, name="detail"),
     # - 리뷰 작성 하기
+    path("reviews/new", views.new, name="new"),
     path("reviews/create", views.create, name="create"),
     # - 리뷰 수정하기
+    path("reviews/<int:review_id>/edit", views.edit, name="edit"),
+    path("reviews/<int:review_id>/update", views.update, name="update"),
     # - 리뷰 삭제하기
+    path("review/<int:review_id>/delete", views.delete, name="delete"),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -14,8 +14,41 @@ def detail(request, review_id):
     return render(request, "reviews/detail.html", context)
 
 
+def new(request):
+    return render(request, "reviews/new.html")
+
+
 def create(request):
     title = request.GET.get("title")
     content = request.GET.get("content")
     Review.objects.create(title=title, content=content)
-    return redirect("/reviews")
+    return redirect("index")
+
+
+def edit(request, review_id):
+    review = Review.objects.get(id=review_id)
+    context = {
+        "review": review,
+    }
+
+    return render(request, "reviews/edit.html", context)
+
+
+def update(request, review_id):
+    review = Review.objects.get(id=review_id)
+    title = request.GET.get("title")
+    content = request.GET.get("content")
+
+    review.title = title
+    review.content = content
+
+    review.save()
+
+    return redirect("detail", review.id)
+
+
+def delete(request, review_id):
+    review = Review.objects.get(id=review_id)
+    review.delete()
+
+    return redirect("index")
